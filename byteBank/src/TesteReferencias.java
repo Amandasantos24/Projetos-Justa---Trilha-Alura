09b59a16fcf2969840ad4db98305a382691b52d0
Amanda Santos <amanda.santos@justa.com.vc>
2022-04-26 15:33:09 -0300

    fazendo alterações de conta

--- byteBank/src/TesteReferencias.java
+++ byteBank/src/TesteReferencias.java
@@ -1,14 +1,14 @@
 //para eu poder chamar a CriaConta eu precisei criar um outro arquivo
 //de java para poder chamar
-
-
-
 public class TesteReferencias {
     public static void main(String[] args) {
         Conta primeiraConta = new Conta();
-                primeiraConta.saldo = 300;
+
+        primeiraConta.saldo = 300;
         System.out.println("saldo da primeira: " + primeiraConta.saldo);
-    Conta segundaConta = primeiraConta;
+
+
+        Conta segundaConta = primeiraConta;
 
         System.out.println("saldo da segunda conta: " + segundaConta.saldo);
 
@@ -17,5 +17,14 @@ public class TesteReferencias {
 
         System.out.println(primeiraConta.saldo);
 
+        segundaConta.agencia = 146;
+        System.out.println("agora a segunda conta esta" + "na agencia" + segundaConta.agencia);
+
+        if(primeiraConta == segundaConta) {
+            System.out.println("são a mesmissima conta");
+
+        } else {
+            System.out.println("contas diferentes");
+        }
     }
 }
